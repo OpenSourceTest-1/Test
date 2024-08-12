87c83fbea513d52075ed8c2a10bc579ca47e6319
mick243 <kjm3746@gmail.com>
2024-08-12 18:51:26 +0900

    first commit

---
+++ .github/pull_request_template.md
@@ -0,0 +1,8 @@
+# minidash pull request 양식
+
+## PR 작업
+
+## 설명
+
+## TODO
+- [ ] 할일
---
+++ index.js
@@ -0,0 +1,7 @@
+export { isArr } from "./src/isArr";
+export { findOddNums } from "./src/findOddNums";
+export { findEvenNums } from "./src/findEvenNums";
+export { capitalize } from "./src/capitalize"; 
+export { isLower } from "./src/isLower";
+export { isUpper } from "./src/isUpper";
+export { difference } from "./src/difference";
